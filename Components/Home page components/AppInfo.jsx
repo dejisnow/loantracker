ffc72d7a0b3e415f46e6ad177dc832a415b3e405
DEJI_SNOW <deji.snow@gmail.com>
2023-11-26 22:45:27 +0100

    new commit

--- Components/Home page components/AppInfo.jsx
+++ Components/Home page components/AppInfo.jsx
@@ -4,16 +4,20 @@ import Image from 'next/image'
 export const AppInfo = () => {
   return (
     <div>
-        <div className='flex h-screen relative'>
+        <div className='h-screen flex '>
+
+
           
-            <div className='info w-80 flex absolute right-2 bottom-0  shadow-2xl justify-center bg-slate-50'>
+            <div className='info w-full h-50 shadow-2xl '>
             <Image  src='/loanImg4.jpg' width={350} height={350}  quality={100} alt='info' />
 
             </div>
-            <div className='info w-80 flex absolute right-6 top-10 shadow-2xl justify-center bg-slate-50'>
+
+         
+            {/* <div className='info w-80 flex absolute right-6 top-10 shadow-2xl justify-center bg-slate-50'>
             <Image  src='/loanImg4.jpg' width={350} height={350}  quality={100} alt='info' />
 
-            </div>
+            </div> */}
 
             
         </div>
