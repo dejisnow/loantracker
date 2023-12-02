fe51ac0dbb010dbc086d5b5b004d60fcbfcca10e
DEJI_SNOW <deji.snow@gmail.com>
2023-12-02 11:15:37 +0100

    update

--- Components/Home page components/AppInfo.jsx
+++ Components/Home page components/AppInfo.jsx
@@ -3,24 +3,38 @@ import Image from 'next/image'
 
 export const AppInfo = () => {
   return (
-    <div>
-        <div className='h-screen flex '>
-
-
-          
-            <div className='info w-full h-50 shadow-2xl '>
-            <Image  src='/loanImg4.jpg' width={350} height={350}  quality={100} alt='info' />
-
+    <div className='flex flex-col'>
+        <div className='h-screen flex-col flex md:flex-row relative '>
+        <div className=' my-10  text-justify flex-50'>
+            <div>
+                <h2 className='text-xl my-4 text-center font-bold'>Check it out</h2>
             </div>
 
-         
-            {/* <div className='info w-80 flex absolute right-6 top-10 shadow-2xl justify-center bg-slate-50'>
-            <Image  src='/loanImg4.jpg' width={350} height={350}  quality={100} alt='info' />
+            <div className='px-4 '>
+            <p >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Libero, maiores vero? Cumque praesentium expedita, delectus libero perspiciatis nobis vitae earum iusto velit qui in illum, consectetur ullam pariatur quos eaque.
+            </p>
+            <div className='my-4'>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur libero animi deleniti 
+                doloremque dolorem ipsa obcaecati ad? Dolores, totam nemo.</p>
+            </div>
 
-            </div> */}
+            </div>
+          
+          </div>
+
+          
+            <div className='info hidden md:flex flex-50 justify-center '>
+            <Image  src='/loanImgCar.jpg' className='rounded-lg top-20 absolute' style={{objectFit:'contain'}} width={350} height={350}  quality={100} alt='info' />
+
+            </div>
+         
+     
 
             
         </div>
+      
 
     </div>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -100,6 +100,7 @@ export default function Home() {
         </div>
           <div>
           <AppInfo />
+          
           </div>
 
       </section>
