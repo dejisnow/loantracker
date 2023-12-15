f9ae8984d593d0ae787217c85630e979c7dc2246
DEJI_SNOW <deji.snow@gmail.com>
2023-12-15 20:39:02 +0100

    update

--- Components/Home page components/AppInfo.jsx
+++ Components/Home page components/AppInfo.jsx
@@ -59,6 +59,37 @@ export const AppInfo = () => {
      
 
     </div>
+    {/* Dashboard Overview:
+Provide a snapshot of the user's overall financial health, including the total outstanding loan amount, upcoming payments, and any important alerts or notifications.
+
+Loan Summary:
+Break down each active loan with key details such as the loan amount, interest rate, term, and current status. This section can help users get a quick overview of their individual loans.
+
+Repayment Management:
+As mentioned before, this section can include features to track and manage repayments, view payment history, set up reminders, and monitor the progress of clearing each installment.
+
+Financial Calendar:
+Display a calendar highlighting important dates such as upcoming payments, loan maturity dates, and other financial events. This can help users plan and budget effectively.
+
+Credit Score Monitoring:
+If applicable, incorporate a section where users can monitor their credit score. This can provide additional insights into their financial standing and help them understand how their loans impact their creditworthiness.
+
+Savings Goals:
+Allow users to set and track savings goals alongside their loan repayments. This could help them balance paying off debts while also working towards other financial objectives.
+
+Educational Resources:
+Offer resources such as articles, tips, and tools to educate users about financial management, loan terms, and smart borrowing practices.
+
+Customer Support and FAQs:
+Provide easy access to customer support and a section for frequently asked questions to assist users in getting quick answers to common queries.
+
+Notifications and Alerts:
+Enable users to customize their notification preferences, such as receiving alerts for upcoming payments, low balances, or changes in interest rates.
+
+Security and Privacy:
+Highlight the security measures in place to protect user data and reassure them about the confidentiality of their financial information.
+
+Remember to design the home page with a user-centric approach, ensuring that the most critical information is easily accessible and the interface is intuitive for users to navigate. */}
       
 
     </div>
